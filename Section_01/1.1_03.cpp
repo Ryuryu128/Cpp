6219c04deaa0b6df0bf68b652c5b821dfefa843d
Ryuryu128 <79790536+Ryuryu128@users.noreply.github.com>
2022-12-12 15:24:42 +0900

    1.1_03 변수와 메모리 -1

---
+++ Section_01/1.1_03.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int x;
+    x = 123; // assignment 대입
+
+    std::cout << x << std::endl;
+    std::cout << &x << std::endl;  // & ampersand
+
+    return 0;
+}
