8882080a9a7da6e66221f0f2415518f241434297
Ryuryu128 <79790536+Ryuryu128@users.noreply.github.com>
2022-12-12 16:01:56 +0900

    Clean up Files

--- Section_01/1.1_04.cpp
+++ Section_01/1.1_04.cpp
@@ -9,6 +9,7 @@ int main()
 {
     // using namespace std; 를 선언하면 선언된 {}내부에서는 std::를 쓰지 않아도 컴파일러가 알아서 파악하고 코드를 실행시켜준다.
     
+    // cout를 이용한 출력
     int x = 1024;
     double pi = 3.141592;
 
@@ -17,6 +18,7 @@ int main()
     std::cout << "abc" << "\t" << "def" << std::endl;
     std::cout << "ab" << "\t" << "cdef" << std::endl;
 
+    // cin을 이용한 입력
     using namespace std;
 
     int y;
